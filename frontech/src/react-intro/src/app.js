9831bd4d1b8b61eaab6ff6a9d4d6c861884d8adf
Leonid2S <sun__2550@hotmail.com>
2016-10-15 12:14:35 +0700

    up

---
+++ frontech/src/react-intro/src/app.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+const SearchForm = () => {
+    return (
+        <form>
+            <input type="text" />
+            <button type="submit">search</button>
+        </form>
+    )
+}
+
+const Header = (props) => (
+    <header>
+        <h1>{props.title}</h1>
+        <SearchForm />
+    </header>
+)
+const Items = (props) => {
+    console.log(props.items)
+    return (
+        <ul>
+        {props.items.map(item => (
+            <li>{item}</li>
+        ))}
+        </ul>
+    )
+}
+
+const Content = (props) => (
+    <section>
+        <p>{props.description}</p>
+        <Items items={props.items}/>
+    </section>
+)
+
+const AppWithoutDescription = () => (
+    <Header title="No description here" />
+)
+
+const App = () => {
+    const appTitle = 'Fronttechs: React'
+    const description = 'This is a simple react application'
+    const items =["Urek","Endrosie","Lo Po Bia Ren","Charie","Lucky"]
+    return (
+        <section>
+            <Header title={appTitle} />
+            <Content description={description} 
+                     items = {items}/>
+        </section>
+    ) 
+}
+const element = document.getElementById('app')
+ReactDOM.render(<App />,element)
